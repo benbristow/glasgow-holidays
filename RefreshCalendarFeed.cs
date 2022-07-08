33c3efcb1e60d64e14d9f7298c58961c172233c0
Ben Bristow <ben@bristow.me>
2022-07-08 19:09:37 +0100

    refactor: Timer job

---
+++ RefreshCalendarFeed.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Azure.Storage.Blobs;
+using HtmlAgilityPack;
+using HtmlAgilityPack.CssSelectors.NetCore;
+using Ical.Net;
+using Ical.Net.CalendarComponents;
+using Ical.Net.DataTypes;
+using Ical.Net.Serialization;
+using Microsoft.Azure.WebJobs;
+
+namespace GlasgowHolidays;
+
+public static class RefreshCalendarFeed
+{
+    [FunctionName(nameof(RefreshCalendarFeed))]
+    public static async Task RunAsync(
+        [TimerTrigger(
+            "0 0 0 * * *"
+#if DEBUG
+            , RunOnStartup=true
+#endif
+            )] TimerInfo timerInfo)
+    {
+        var doc = await GetHtmlDocumentAsync("https://www.glasgow.gov.uk/article/3741/Public-General-Holidays");
+        var calendar = GetCalendar(doc);
+        await UpdateBlobStorageAsync(calendar);
+    }
+
+    private static async Task<HtmlDocument> GetHtmlDocumentAsync(string url)
+    {
+        var client = new HttpClient();
+        var document = new HtmlDocument();
+        var html = await client.GetStringAsync(url);
+        document.LoadHtml(html);
+        return document;
+    }
+
+    private static Calendar GetCalendar(HtmlDocument doc)
+    {
+        var calendar = new Calendar();
+        calendar.Events.AddRange(doc.QuerySelectorAll("table[summary=holidays] tr")
+            .SelectMany(row =>
+            {
+                var datesColumn = row.QuerySelector("td:nth-child(3)");
+                var datesColumnPTags = datesColumn.QuerySelectorAll("p");
+                var dateNodes = datesColumnPTags.Any() ? datesColumnPTags : new[] { datesColumn };
+
+                return dateNodes.Select(dateTextNode => new CalendarEvent
+                {
+                    Summary = HtmlEntity.DeEntitize(row.QuerySelector("td:nth-child(1) td").InnerText),
+                    Start = new CalDateTime(DateTime.Parse(
+                        HtmlEntity.DeEntitize(dateTextNode.InnerText),
+                        System.Globalization.CultureInfo.InvariantCulture)),
+                    IsAllDay = true
+                });
+            }));
+        
+        return calendar;
+    }
+
+    private static async Task UpdateBlobStorageAsync(Calendar calendar)
+    {
+        var blobContainerClient = new BlobContainerClient(
+            Environment.GetEnvironmentVariable("AZURE_STORAGE_CONNECTION_STRING"),
+            Environment.GetEnvironmentVariable("AZURE_STORAGE_BLOB_CONTAINER_NAME"));
+
+        var blobClient = blobContainerClient.GetBlobClient("glasgowHolidays.ics");
+
+        await blobClient.UploadAsync(
+            BinaryData.FromString(new CalendarSerializer().SerializeToString(calendar)),
+            overwrite: true);
+    }
+}
